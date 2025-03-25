402281816ebdd16d1dca77c592a670a57f59be50
Avotraina <avotrainarabe@gmail.com>
2025-03-25 12:26:30 +0300

    add content

--- app/_components/experience/experience.tsx
+++ app/_components/experience/experience.tsx
@@ -54,6 +54,71 @@ export default function Experience() {
             `,
             techs: ['Angular', 'Angular Material', 'JavaScript', 'TypeScript', 'Laravel', 'Mysql'],
         },
+        {
+            company: "Relia consulting - Fullstack developer",
+            position: ["Confidential internal project"],
+            date: "Date 4",
+            description: `
+            
+                A super app designed to integrate multiple services within a single platform, similar to Amazon
+            
+                My role envolved to share best practice to the team too.
+
+            `,
+            techs: ['Angular', 'Angular Material', 'JavaScript', 'TypeScript', 'NextJS', 'React', 'NestJS', 'Spring Boot', 'MongoDB'],
+        },
+        {
+            company: "Relia consulting - Laravel developer",
+            position: ["Confidential project"],
+            date: "Date 4",
+            description: `
+            
+                An intranet fisheries information management system designed to enhance and expedite the exchange of information among fisheries authorities.
+
+                Joined an existing cross-functional development team as a Laravel developer to contribute to the ongoing project.
+            
+            `,
+            techs: ['Laravel', 'AlpineJS', 'Mysql'],
+        },
+        {
+            company: "Relia consulting - Laravel developer",
+            position: ["Confidential project"],
+            date: "Date 4",
+            description: `
+            
+                An intranet fisheries information management system designed to enhance and expedite the exchange of information among fisheries authorities.
+
+                Joined an existing cross-functional development team as a Laravel developer to contribute to the ongoing project.
+            
+            `,
+            techs: ['Laravel', 'AlpineJS', 'Mysql'],
+        },
+        {
+            company: "Relia consulting - React Native developer",
+            position: ["Confidential project"],
+            date: "Date 4",
+            description: `
+            
+                A social networking mobile app designed for poker enthusiasts worldwide. The platform allows players to connect, share upcoming poker events, and discover local or online poker parties.
+
+                Joined an existing cross-functional development team as a React Native developer.
+            
+            `,
+            techs: ['React Native'],
+        },
+        {
+            company: "Relia consulting - Fullstack developer",
+            position: ["Confidential project"],
+            date: "Date 4",
+            description: `
+            
+                A professional networking app that allows users to share job opportunities and book interviews.
+
+                I handled both frontend and backend tasks simultaneously
+
+            `,
+            techs: [''],
+        },
     ]
 
     return (
