71951a4f547d39b6aa6a55aafb90360b677e50f8
Avotraina <avotrainarabe@gmail.com>
2025-04-12 10:22:30 +0300

    optimize metadata

---
+++ app/[locale]/sitemap.xml/route.ts
@@ -0,0 +1,34 @@
+// app/[locale]/sitemap.xml/route.ts
+import { type NextRequest } from 'next/server'
+
+export const dynamic = 'force-static' // optional for caching
+
+export async function GET(req: NextRequest) {
+  const baseUrl = 'https://avotraina-rabe.vercel.app'
+
+  const locales = ['en', 'fr'] // tes langues supportées
+
+  const urls = [
+    '', // homepage
+    '/projects',
+    '/about',
+    '/contact',
+  ]
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${locales
+  .flatMap((locale) =>
+    urls.map(
+      (path) => `<url><loc>${baseUrl}/${locale}${path}</loc></url>`
+    )
+  )
+  .join('\n')}
+</urlset>`
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
+}
