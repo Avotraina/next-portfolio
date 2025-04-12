7ddcfce049973683443d4cad886701527e83084c
Avotraina <avotrainarabe@gmail.com>
2025-04-12 13:52:06 +0300

    optimize metadata

--- app/robots.ts
+++ app/robots.ts
@@ -5,8 +5,8 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
-            disallow: "/private/",
+            disallow: "/_next/",
         },
-        sitemap: "https://avotraina-rabe.vercel.app/en/sitemap.xml",
+        sitemap: "https://avotraina-rabe.vercel.app/sitemap.xml",
     }
 }
