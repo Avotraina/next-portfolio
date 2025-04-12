3646890915e0a8fba6615feaca400d2bcd03e236
Avotraina <avotrainarabe@gmail.com>
2025-04-12 11:23:15 +0300

    optimize metadata

--- app/[locale]/sitesssmap.ts
+++ app/[locale]/sitesssmap.ts
@@ -10,6 +10,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
         {
             url: "https://avotraina-rabe.vercel.app",
             lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+            alternates: {
+                languages: {
+                    en: "https://avotraina-rabe.vercel.app",
+                    fr: "https://avotraina-rabe.vercel.app/fr",
+                }
+            }
         },
         // {
         //     url: "https://avotraina-rabe.vercel.app/projects",
