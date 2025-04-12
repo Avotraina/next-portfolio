74bf709fcc83cef71a4571bff2d4f722368bc058
Avotraina <avotrainarabe@gmail.com>
2025-04-12 16:25:28 +0300

    optimize metadata

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,8 @@
 import { getScopedI18n } from "@/locales/server";
 import About from "./_components/about/About";
-import Contact from "./_components/contact/contact";
-import Experience from "./_components/experience/experience";
-import Projects from "./_components/projects/projects";
+// import Contact from "./_components/contact/contact";
+// import Experience from "./_components/experience/experience";
+// import Projects from "./_components/projects/projects";
 
 export default async function Home() {
 
@@ -12,9 +12,9 @@ export default async function Home() {
         // <div className="">
         <main className="flex flex-col flex-1 py-24 pt-24 lg:w-[52%] lg:py-24 gap-4 max-w-screen-md">
             <About />
-            <Experience />
+            {/* <Experience />
             <Projects />
-            <Contact />
+            <Contact /> */}
             <div className="border-t pt-4 mt-4 px-4 block lg:hidden">
                 <span className="text-xs">
                     {footer("description")}
