4d93e1fa78cac33f042853ebe69dd094a36475a7
Avotraina <avotrainarabe@gmail.com>
2025-04-12 17:17:15 +0300

    optimize metadata

--- app/_components/pannel/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="border-t pt-4 px-4 hidden lg:block">
-            <span className="text-xs">
-                Developed with NextJS and Tailwind CSS
-            </span>
-        </div>
-    );
-}
